6f0bf46ae48b39828533d2fff8b5104f3abb877d
fengwenbo <862481966@qq.com>
2022-04-16 21:49:16 -0700

    epoll oneshot

--- Test/io_epoll_oneshot_server.cpp
+++ Test/io_epoll_oneshot_server.cpp
@@ -1,13 +1,19 @@
-#include <iostream>
 #include <sys/socket.h>
 #include <arpa/inet.h>
-#include <strings.h>
 #include <sys/epoll.h>
-#include <unistd.h>
-#include <memory.h>
+#include <pthread.h>
 #include <fcntl.h>
+#include <unistd.h>
+#include <iostream>
 
 #define MAX_EVENT_NUM 1024
+#define BUFFER_SIZE 1024
+
+struct fds
+{
+    int epoll_fd;
+    int sock_fd;
+};
 
 void add_fd_into_epoll_fds(int epollfd,int tarfd){
     //add fd to epoll
@@ -21,21 +27,27 @@ void add_fd_into_epoll_fds(int epollfd,int tarfd){
     fcntl(tarfd,F_SETFL,new_option);
 }
 
+void* worker(void* arg){
+    return NULL;
+}
+
 int main(){
     int listen_fd=socket(AF_INET,SOCK_STREAM,0);
-    if(listen_fd==-1){
+    if(listen_fd<0){
         perror("socket error\n");
         return -1;
     }
+
     struct sockaddr_in listen_addr;
     listen_addr.sin_family=AF_INET;
-    listen_addr.sin_port=htons(9999);
     listen_addr.sin_addr.s_addr=INADDR_ANY;
-    int ret=bind(listen_fd,(const struct sockaddr*)&listen_addr,sizeof(listen_addr));
+    listen_addr.sin_port=htons(9999);
+    int ret=bind(listen_fd,(const sockaddr*)&listen_addr,sizeof(listen_addr));
     if(ret==-1){
         perror("bind error\n");
         return -1;
     }
+
     ret=listen(listen_fd,5);
     if(ret==-1){
         perror("listen error\n");
@@ -59,29 +71,21 @@ int main(){
         }
         for(int i=0;i<ret;i++){
             int sock_fd=events[i].data.fd;
-            if(sock_fd==listen_fd){//events of listen_fd invoke,which means client connect,so we should add client_fd(conn_fd) into the epoll-lists
+            if(sock_fd==listen_fd){
                 struct sockaddr_in client_addr;
                 socklen_t len=sizeof(client_addr);
-                int conn_fd=accept(sock_fd,(sockaddr*)&client_addr,&len);
-                add_fd_into_epoll_fds(epoll_fd,conn_fd);
+                int conn_fd=accept(listen_fd,(sockaddr*)&client_addr,&len);
+                add_fd_into_epoll_fds(epoll_fd,sock_fd);
             }
-            else if(events[i].events&EPOLLIN){//message read ready
-                char buf[1024];
-                memset(buf,'\0',sizeof(buf));
-                ret=recv(sock_fd,buf,sizeof(buf)-1,0);
-                if(ret<0){
-                    close(sock_fd);
-                    break;
-                }
-                else if(ret==0){
-                    break;
-                }
-                else{
-                    printf("client data:%s\n",buf);
-                }
+            else if(events[i].events&EPOLLIN){
+                pthread_t thread;
+                fds fd_new_thread;
+                fd_new_thread.epoll_fd=epoll_fd;
+                fd_new_thread.sock_fd=sock_fd;
+                pthread_create(&thread,NULL,worker,&fd_new_thread);
             }
             else{
-                printf("something else happened.\n");
+                printf("something else happened\n");
             }
         }
     }
