9c5346091716ae686c49181926d20b1f89bd084a
fengwenbo <862481966@qq.com>
2022-07-08 05:39:33 -0700

    lsm_tree

--- Test/lsm_tree/lsm.h
+++ Test/lsm_tree/lsm.h
@@ -3,11 +3,20 @@
 
 template<class K,class V>
 class LSM{
-private:
+public:
+    ~LSM<K,V>();
     LSM<K,V>(const LSM<K,V> &other) = default;
     LSM<K,V>(LSM<K,V> &other) = default;
     LSM<K,V>(unsigned long eltsPerRun,unsigned int numRuns, double merged_frac,double bf_fp,unsigned int pageSize,unsigned int diskRunPerLevel);
-public:
+    void insert_key(K &key,V &value);
+    void delete_key(K &key);
+    bool lookup(K &key,V &value);
+private:
+    void mergeRunsToLevel(int level);
+    //void merge_runs(vector<Run<K,V>*> runs_to_merge,vector<>)
+    void do_merge();
+    unsigned long num_buffer();
+    unsigned long size();
 };
 
 #endif
