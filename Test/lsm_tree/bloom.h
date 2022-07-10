4c0ed30d0be2344975ed509cfa97c3f58985fc6c
fengwenbo <862481966@qq.com>
2022-07-09 22:46:55 -0700

    lsm-tree-0.1.1 bloomfitler

--- Test/lsm_tree/bloom.h
+++ Test/lsm_tree/bloom.h
@@ -1,4 +1,19 @@
 #ifndef BLOOM_H
 #define BLOOM_H
 
+#include <stdio.h>
+#include <cstdint>
+#include <vector>
+#include <array>
+#include <math.h>
+
+using namespace std;
+template<class Key>
+class BloomFilter{
+public:
+    BloomFilter(uint64_t n,double fp);
+private:
+    vector<bool> bits_;
+};
+
 #endif
