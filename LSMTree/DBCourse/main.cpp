63e898de1e2fd79e4c029ecc19f4bb8f49980776
fengwenbo <862481966@qq.com>
2023-01-02 09:34:51 -0800

    dbcourse

---
+++ LSMTree/DBCourse/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    std::cout << "input function:" << std::endl;
+    std::cout << "1.new" << std::endl;
+    std::cout << "2.select" << std::endl;
+    int func_type;
+    std::cin >> func_type;
+    switch (func_type)
+    {
+    case 1:
+        break;
+    case 2:
+        break;
+    default:
+        break;
+    }
+}
