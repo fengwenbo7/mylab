cb0c98e3a0c9f67f3810efe3119583bf411feaa1
fengwenbo <862481966@qq.com>
2023-01-21 07:20:30 -0800

    save/search function

--- DBCourse/ContanceManager.h
+++ DBCourse/ContanceManager.h
@@ -5,6 +5,8 @@
 #include <string>
 #include "ContactInfo.h"
 #include <fstream>
+#include <list>
+#include <algorithm>
 using namespace std;
 class ContactManager
 {
@@ -13,6 +15,11 @@ public:
     {
         file_off_ = 0;
         data_size_ = 0;
+        fstream fs(dat_file, ios::out);
+        if (fs)
+        {
+            fs.close();
+        }
     }
 
     ~ContactManager()
@@ -21,6 +28,7 @@ public:
 
     void SaveData(PersonInfo info)
     {
+        // save origin data
         fstream dat_file_s(dat_file_path_, ios::in | ios::out | ios::binary);
         if (!dat_file_s)
         {
@@ -33,6 +41,8 @@ public:
         data_size_++;
         std::cout << "save data to dat success,file_off:" << file_off_ << ",data_size:" << data_size_ << std::endl;
         dat_file_s.close();
+
+        // save bplus tree
     }
 
     size_t ReadFromDat(PersonInfo *data)
@@ -54,6 +64,52 @@ public:
         dat_file_s.close();
         std::cout << "fclose ok." << std::endl;
         return data_size_;
+    }
+
+    list<PersonInfo> SearchFromDat(const char *name, const char *phonenum, const char *email)
+    {
+        list<PersonInfo> ret;
+        fstream dat_file_s(dat_file_path_, ios::in | ios::out | ios::binary);
+        if (!dat_file_s)
+        {
+            std::cout << "fopen error:" << dat_file_path_ << std::endl;
+            return ret;
+        }
+        dat_file_s.seekg(0, ios::beg);
+        PersonInfo p1[data_size_];
+        dat_file_s.read((char *)p1, sizeof(PersonInfo) * data_size_);
+        string input_name(name);
+        string input_phonenum(phonenum);
+        string input_email(email);
+        transform(input_name.begin(), input_name.end(), input_name.begin(), ::tolower);
+        transform(input_phonenum.begin(), input_phonenum.end(), input_phonenum.begin(), ::tolower);
+        transform(input_email.begin(), input_email.end(), input_email.begin(), ::tolower);
+        for (size_t i = 0; i < data_size_; i++)
+        {
+            string tmp_name(p1[i].name);
+            string tmp_phonenum(p1[i].phonenumber);
+            string tmp_email(p1[i].email);
+            transform(tmp_name.begin(), tmp_name.end(), tmp_name.begin(), ::tolower);
+            transform(tmp_phonenum.begin(), tmp_phonenum.end(), tmp_phonenum.begin(), ::tolower);
+            transform(tmp_email.begin(), tmp_email.end(), tmp_email.begin(), ::tolower);
+            if (name != nullptr && strcmp(tmp_name.c_str(), input_name.c_str()) != 0)
+            {
+                continue;
+            }
+            if (phonenum != nullptr && strcmp(tmp_phonenum.c_str(), input_phonenum.c_str()) != 0)
+            {
+                continue;
+            }
+            if (email != nullptr && strcmp(tmp_email.c_str(), input_email.c_str()) != 0)
+            {
+                continue;
+            }
+            ret.push_back(p1[i]);
+        }
+
+        dat_file_s.close();
+        std::cout << "fclose ok." << std::endl;
+        return ret;
     }
 
 private:
--- DBCourse/main.cpp
+++ DBCourse/main.cpp
@@ -1,6 +1,124 @@
+#include <iostream>     // std::cout、std::endl
+#include <sys/socket.h> // socket、bind、listen、accept
+#include <netinet/in.h> // htonl、htons
+#include <unistd.h>     // write、close
+#include <string.h>     // bzero
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <arpa/inet.h>
+#include <strings.h>
+#include <sys/epoll.h>
+#include <memory.h>
+#include <fcntl.h>
+
 #include <iostream>
 #include "ContanceManager.h"
 using namespace std;
+
+#define PORT 8099
+#define LISTENQ 5
+#define MAXLINE 4096
+#define MAX_EVENT_NUM 1024
+
+void add_fd_into_epoll_fds(int epollfd, int tarfd)
+{
+    // add fd to epoll
+    epoll_event event;
+    event.data.fd = tarfd;
+    event.events = EPOLLIN | EPOLLET | EPOLLONESHOT;
+    epoll_ctl(epollfd, EPOLL_CTL_ADD, tarfd, &event);
+    // set non-block
+    int old_option = fcntl(tarfd, F_GETFL);
+    int new_option = old_option | O_NONBLOCK;
+    fcntl(tarfd, F_SETFL, new_option);
+}
+
+int _main()
+{
+    int listen_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (listen_fd == -1)
+    {
+        perror("socket error\n");
+        return -1;
+    }
+    struct sockaddr_in listen_addr;
+    listen_addr.sin_family = AF_INET;
+    listen_addr.sin_port = htons(PORT);
+    listen_addr.sin_addr.s_addr = INADDR_ANY;
+    int ret = bind(listen_fd, (const struct sockaddr *)&listen_addr, sizeof(listen_addr));
+    if (ret == -1)
+    {
+        perror("bind error\n");
+        return -1;
+    }
+    ret = listen(listen_fd, 5);
+    if (ret == -1)
+    {
+        perror("listen error\n");
+        return -1;
+    }
+
+    epoll_event events[MAX_EVENT_NUM];
+    int epoll_fd = epoll_create(5);
+    if (epoll_fd < 0)
+    {
+        perror("epoll create error\n");
+        return -1;
+    }
+
+    add_fd_into_epoll_fds(epoll_fd, listen_fd);
+
+    while (1)
+    {
+        ret = epoll_wait(epoll_fd, events, MAX_EVENT_NUM, -1);
+        if (ret < 0)
+        {
+            perror("epoll wait error\n");
+            break;
+        }
+        for (int i = 0; i < ret; i++)
+        {
+            int sock_fd = events[i].data.fd;
+            if (sock_fd == listen_fd)
+            {
+                // events of listen_fd invoke,which means client connect,so we should add client_fd(conn_fd) into the epoll-lists
+                struct sockaddr_in client_addr;
+                socklen_t len = sizeof(client_addr);
+                int conn_fd = accept(sock_fd, (sockaddr *)&client_addr, &len);
+                add_fd_into_epoll_fds(epoll_fd, conn_fd);
+            }
+            else if (events[i].events & EPOLLIN)
+            {
+                // message read ready
+                char buf[1024];
+                memset(buf, '\0', sizeof(buf));
+                ret = recv(sock_fd, buf, sizeof(buf) - 1, 0);
+                if (ret < 0)
+                {
+                    close(sock_fd);
+                    break;
+                }
+                else if (ret == 0)
+                {
+                    break;
+                }
+                else
+                {
+                    printf("client data:%s\n", buf);
+                }
+            }
+            else
+            {
+                printf("something else happened.\n");
+            }
+        }
+    }
+    close(listen_fd);
+    return 0;
+}
+
 int main()
 {
     ContactManager cm("ContactDat.dat");
@@ -33,24 +151,35 @@ int main()
         }
         break;
         case 2:
-            break;
+        {
+            std::cout << "search name:";
+            string in_name;
+            std::cin >> in_name;
+
+            std::cout << "search phone number:";
+            string in_phonenum;
+            std::cin >> in_phonenum;
+
+            std::cout << "search email:";
+            string in_email;
+            std::cin >> in_email;
+
+            list<PersonInfo> results = cm.SearchFromDat(in_name.c_str(), in_phonenum.c_str(), in_email.c_str());
+            for (auto iter = results.begin(); iter != results.end(); iter++)
+            {
+                std::cout << "name:" << iter->name << " "
+                          << "age:" << iter->age << " "
+                          << "phonenumber" << iter->phonenumber << " "
+                          << "email" << iter->email << std::endl;
+            }
+        }
+        break;
         case 3:
         {
             PersonInfo *data;
             size_t data_len_ = cm.ReadFromDat(data);
             int index = 0;
             std::cout << "read from dat:" << data_len_ << std::endl;
-            /*if (data_len_ > 0)
-            {
-                for (size_t i = 0; i < data_len_; i++)
-                {
-                    std::cout << "name:" << data->name << " "
-                              << "age:" << data->age << " "
-                              << "phonenumber" << data->phonenumber << " "
-                              << "email" << data->email << std::endl;
-                    data = data + sizeof(pi);
-                }
-            }*/
         }
         break;
         default:
